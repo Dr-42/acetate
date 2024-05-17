c253749f9b00a50847f848e9a52bc8b583868cfc
Spandan Roy <royspandan1@gmail.com>
2024-05-17 22:38:59 +0530

    Create logical device

--- engine/vk_man/ac_vulkan.c
+++ engine/vk_man/ac_vulkan.c
@@ -1,5 +1,6 @@
 #include "vk_man/ac_vulkan.h"
 
+#include <stdint.h>
 #include <vulkan/vk_enum_string_helper.h>
 #include <vulkan/vulkan.h>
 
@@ -35,12 +36,14 @@ VkInstance create_instance(const char* app_name, ac_vk_data* vk_data);
 VkSurfaceKHR create_surface(ac_vk_data* vk_data);
 void setup_debug_messenger(ac_vk_data* vk_data);
 void create_physical_device(ac_vk_data* vk_data);
+void create_logical_device(ac_vk_data* vk_data);
 
 void init_vulkan(const char* app_name, ac_vk_data* vk_data) {
     vk_data->instance = create_instance(app_name, vk_data);
     vk_data->surface = create_surface(vk_data);
     setup_debug_messenger(vk_data);
     create_physical_device(vk_data);
+    create_logical_device(vk_data);
 }
 
 VkInstance create_instance(const char* app_name, ac_vk_data* vk_data) {
@@ -189,6 +192,71 @@ void create_physical_device(ac_vk_data* vk_data) {
     ac_log_fatal_exit("Failed to find suitable GPU\n");
 }
 
+void create_logical_device(ac_vk_data* vk_data) {
+    queue_family_indices_t indicies = {0};
+    find_queue_families(&vk_data->physical_device, &indicies, &vk_data->surface);
+
+    ac_darray_t* unique_queue_families = ac_darray_create(sizeof(uint32_t), 3, AC_MEM_ENTRY_VULKAN);
+    uint32_t queue_families[] = {indicies.graphics_family, indicies.present_family, indicies.transfer_family};
+    for (size_t i = 0; i < ARRAY_SIZE(queue_families); i++) {
+        bool found = false;
+        for (size_t j = 0; j < unique_queue_families->size; j++) {
+            uint32_t queue_family;
+            ac_darray_get(unique_queue_families, j, &queue_family);
+            if (queue_families[i] == queue_family) {
+                found = true;
+                continue;
+            }
+        }
+        if (!found) {
+            ac_darray_push(unique_queue_families, &queue_families[i]);
+        }
+    }
+    ac_darray_t* queue_create_infos =
+        ac_darray_create(sizeof(VkDeviceQueueCreateInfo), unique_queue_families->size, AC_MEM_ENTRY_VULKAN);
+    for (size_t i = 0; i < unique_queue_families->size; i++) {
+        VkDeviceQueueCreateInfo queueCreateInfo = {0};
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = queue_families[i];
+        queueCreateInfo.queueCount = 1;
+        float queuePriority = 1.0f;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+        ac_darray_push(queue_create_infos, &queueCreateInfo);
+    }
+
+    ac_log_debug("Unique queue families: %zu\n", unique_queue_families->size);
+
+    VkPhysicalDeviceFeatures device_features = {0};
+    device_features.samplerAnisotropy = VK_TRUE;
+
+    VkDeviceCreateInfo create_info = {0};
+    create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    create_info.pQueueCreateInfos = (VkDeviceQueueCreateInfo*)queue_create_infos->data;
+    create_info.queueCreateInfoCount = queue_create_infos->size;
+    create_info.pEnabledFeatures = &device_features;
+
+    if (!check_device_extension_support(vk_data->physical_device, device_extensions, ARRAY_SIZE(device_extensions))) {
+        ac_log_fatal_exit("Device does not support required extensions\n");
+    }
+    ac_log_info("Required device extensions supported\n");
+
+    create_info.enabledExtensionCount = ARRAY_SIZE(device_extensions);
+    create_info.ppEnabledExtensionNames = device_extensions;
+
+    if (vk_data->enable_validation_layers) {
+        create_info.enabledLayerCount = ARRAY_SIZE(validation_layers);
+        create_info.ppEnabledLayerNames = validation_layers;
+    } else {
+        create_info.enabledLayerCount = 0;
+    }
+
+    VkResult result = vkCreateDevice(vk_data->physical_device, &create_info, NULL, &vk_data->device);
+    VK_CHECK(result);
+    ac_log_info("Logical device created\n");
+    ac_darray_destroy(queue_create_infos);
+    ac_darray_destroy(unique_queue_families);
+}
+
 void init_swapchain() {}
 void init_command() {}
 void init_sync() {}
@@ -219,6 +287,7 @@ void ac_vk_cleanup(ac_vk_data* vk_data) {
     if (vk_data->enable_validation_layers) {
         destroy_debug_utils_messenger(vk_data->instance, vk_data->debug_messenger, NULL);
     }
+    vkDestroyDevice(vk_data->device, NULL);
     vkDestroySurfaceKHR(vk_data->instance, vk_data->surface, NULL);
     vkDestroyInstance(vk_data->instance, NULL);
     ac_free(vk_data->debug_messenger);
